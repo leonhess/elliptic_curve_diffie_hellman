de6ebfe02d093d844c59c6f8a8470b739fad0e8b
Leon Heß <leonhess423@gmail.com>
2025-01-05 14:23:06 +0100

    Reworking elliptical math

--- elliptical_curves/elliptical_curve.py
+++ elliptical_curves/elliptical_curve.py
@@ -1,23 +1,6 @@
 import secrets
-
-class point:
-    def __init__(self,p_x,p_y,inf=0):
-        self.x = p_x 
-        self.y = p_y
-        self.inf=inf
-
-    def getX(self):
-        return self.x 
-
-    def getY(self):
-        return self.y
-    
-    def is_infinte(self):
-        return self.inf
-
-    def pr(self):
-        print(f"x:{self.getX()} y:{self.getY()}")
-
+from .elliptical_curve_math import math as e_math
+from .point import point
 
 #definition of curve 
 # y**2 = x**3 +A*x +B
@@ -29,93 +12,133 @@ class elliptical_curve:
         
         self.a = p_a #curve parameter
         self.b = p_b #curve parameter 
-        
-        self.G = p_G # Generator Point
         #self.n = p_n # order 
         self.h = p_h # cofactor
+        if self.is_on_curve(p_G):
+            self.G = p_G # Generator Point
+        else:
+            print("Error Generator point not on curve")
 
-    def calculate_secret_point(self):
-        self.public_Point = self.multiply_point(self.G, self.K-1)
+    ## simple curve functions 
+    def is_on_curve(self, P):
+        x = P.getX()
+        y = P.getY()
+        eq1 = e_math.power(y,2,self.p)
+        eq2 = e_math.power(x,3,self.p)
+        eq3 = e_math.multiply(self.a, x, self.p)
+        eq4 = e_math.add(eq2, eq3, self.p)
+        eq5 = e_math.add(eq4, self.b, self.p)
 
-    def calculate_shared_Point(self,P):
-        self.shared_Point = self.multiply_point(P,self.K-1)
+        return eq1 == eq5
+
+    ## point operations on curve 
+    def add_points(self, P1,P2):
+        R = point(0,0,inf=True)
+        if e_math.are_points_equal(P1,P2) and P1.getX()!=0:
+            # use point doubling
+            R = self.point_doubling(P1,P2)
+        elif not e_math.are_points_equal(P1,P2) and P1.getX() != P2.getX():
+            #use normal addition
+            R = self.simple_point_addition(P1,P2)
+        return R
+
+    def point_doubling(self, P1,P2):
+        p = self.p
+        a = self.a
+        # calculate the slope 
+        #s =  (3 * (X1^2) + a) / (2*Y1) 
+        s_z = e_math.power(P1.getX(),2, p)    # X1^2
+        s_z =  e_math.multiply(3, s_z, p)      # 3*(X1^2)
+        s_z = e_math.add(s_z, a, p)            # 3 * (X1^2) + a
+
+        s_n = e_math.multiply(2, P1.getY(), p) # 2*Y1
+     
+        s = e_math.multiply(s_z,e_math.inverse(s_n,p), p)         # (3*(X1^2)+a) / (2*Y1)
+
+        # calculate the new x
+        # x =  s^2 - 2*X1 
+        x = e_math.subtract(e_math.power(s,2,p),            # S^2
+                            e_math.multiply(2,P1.getX(),p), # (2*X1) 
+                            p)
+
+        # calculate new y
+        # y = s*(x1-x)-Y1
+        y = e_math.subtract(e_math.multiply(s, # s
+                                             e_math.subtract(P1.getX(),x, p) # (X1 -x)
+                                             , p),
+                             P1.getY(), # Y1
+                             p) 
+        return point(x,y)
 
 
-    def calculate_cyclic_group(self):
-        n = 1 
-        group = [self.G]
-        while(not group[n-1].is_infinte()):
-            P = self.add_points(self.G,group[n-1])
-            group.append(P)
-            n+=1
-        self.n = n
 
+    def simple_point_addition(self, P1, P2 ):
+        p = self.p
+        # calculate the slope 
+        #s =  (Y1- Y2) / (X1 - X2) 
+        s_z = e_math.subtract(P1.getY(), P2.getY(), p) # Y1 - Y2 
+        s_n = e_math.subtract(P1.getX(), P2.getX(), p) # X1 - X2
+        s = e_math.multiply(s_z,e_math.inverse(s_n, p), p) # (Y1 - Y2) / (X1 - X2) 
+        # calculate the new x
+        # x =  s^2 - (X1+X2 ) 
+        x = e_math.subtract(e_math.power(s, 2, p),               # s^2
+                            e_math.add(P1.getX(), P2.getX(),p),  # (X1 + P2)
+                            p)
+        # calculate new y 
+        # y = s*(X1 -X) - Y1 
+        y = e_math.subtract(e_math.multiply(s,                                  #s 
+                                            e_math.subtract(P1.getX(),x,p),     # (X1-X)
+                                            p),
+                            P1.getY(),                                          # Y1
+                            p)
+        return point(x,y)
+
+
+    def multiply_point(self,P,x):
+        T = P
+        for i in range(0,x):
+            T = self.add_points(T,P)
+        return T
+
+    ## Cryptic functions
     def generate_secret(self):
         self.K = secrets.choice(range(1,self.n))
         self.K = 9
         print(self.K)
 
+    def calculate_secret_point(self):
+        secret_point = self.multiply_point(self.G, self.K-1)
+        if not self.is_on_curve(secret_point):
+            print("secret point error")
+        self.public_Point = secret_point
+        secret_point.pr()
+
+    def calculate_shared_Point(self,P):
+        shared_point = self.multiply_point(P, self.K-1)
+        if not self.is_on_curve(shared_point):
+            print("secret point error")
+        self.shared_Point = shared_point
+        shared_point.pr()
+
+    def calculate_cyclic_group(self):
+        n = 1 
+        group = [self.G]
+        while(not group[n-1].is_infinte()):
+            group[n-1].pr()
+            P = self.add_points(self.G,group[n-1])
+            group.append(P)
+            P.pr()
+            n+=1
+        self.n = n
+        self.cyclic_group = group
+
+
+    ## getter 
     def calculate_cofactor():
         pass 
 
     def get_order(self):
         return self.n
+    
 
-
-
-    def is_on_curve(self, P):
-        x = P.getX()
-        y = P.getY()
-        return y**2 == x**3 + self.a *x + self.b 
-
-    def equal_points(self, P1, P2):
-        if P1.getX() == P2.getX() and P1.getY() == P2.getY():
-            return True 
-        else:
-            return False
-
-        
-
-    def add_points(self, P1,P2):
-        x = 0 
-        y = 0
-        print(f"p2 to add :x = {P2.getX()}, y = {P2.getY()}")
-        if self.equal_points(P1,P2):
-            if P1.getX() != 0:
-                # point doubling
-                print("point double")
-                s_z = (3 * ((P1.getX()**2 )% self.p) + self.a ) % self.p
-                s_n = 2 * P1.getY() 
-                s_n = pow(s_n, -1, self.p)
-               
-                input(s_n)
-                s = (s_z * s_n) % self.p
-
-
-                print(f"slope : {s}")
-            
-                x = ( ((s**2) % self.p) - 2 * P1.getX()) % self.p  
-                y = ( s * (P1.getX() - x) - P1.getY()) % self.p
-            else:
-                return point(0,0,inf=True)
-        else:
-            if P1.getX() != P2.getX():
-                #normal addition
-                s_z = (P1.getY() - P2.getY()) % self.p 
-                s_n =  P1.getX() - P2.getX()
-                s_n = pow(s_n, -1, self.p)
-                s = (s_z * s_n) % self.p
-
-                x = ((s**2)%self.p -(P1.getX() + P2.getX())) % self.p
-                y = ((s * (P1.getX() - x )) % self.p - P1.getY()) % self.p
-            else:
-                return point(0,0,inf=True)
-        new_point = point(x,y)
-        return new_point
-
-    def multiply_point(self,P,x):
-        P.pr()
-        T = P
-        for i in range(0,x):
-            T = self.add_points(T,P)
-        return T
+    
--- elliptical_curves/elliptical_curve_math.py
+++ elliptical_curves/elliptical_curve_math.py
@@ -1,26 +1,37 @@
-def are_points_equal(P1,P2):
-    if P1.getX() == P2.getX() and P1.getY() == P2.getY():
-        return True 
-    else:
-        return False
+class math:
+    def are_points_equal(P1,P2):
+        if P1.getX() == P2.getX() and P1.getY() == P2.getY():
+            return True 
+        else:
+            return False
 
 # basic math operators
-def subtract(x,y):
-    return x-y
+    def subtract(x,y,p):
+        sub = x-y 
+        sub = sub % p
+        return sub
 
-def add(x, y, p ):
-    return x+y
+    def add(x, y, p ):
+        sum_ = x+y 
+        sum_ = sum_ % p
+        return sum_
 
-def multiply(X, Y, p):
-    Z = X*Y 
-    return Z
+    def multiply( X, Y, p):
+        Z = X*Y
+        #Z = Z % p
+        return Z
+    
+    def inverse(N,p):
+        multiplicative_inverse_modul = pow(N,-1,p)
+        return multiplicative_inverse_modul 
 
-def divide(Z,N,p):
-    multiplicative_inverse_modul = pow(N,-1,p)
-    r = multiply(Z,multiplicative_inverse_modul)
-    r = r % p
-    return r 
+    #def divide(self, Z,N,p):
 
-def power(B,E,p):
-    r = B**E % p
-    return r 
+    #    r = self.multiply(Z,multiplicative_inverse_modul)
+    #    r = r % p
+    #    return r 
+
+    def power(B,E,p):
+        r = B**E
+        r = r % p
+        return r 
